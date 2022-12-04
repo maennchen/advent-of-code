84f5d712e325b8c7186f0144214a746ab6ca3174
Jonatan Männchen <jonatan@maennchen.ch>
2022-12-04 17:37:16 +0100

    Add 2022 Day 04

---
+++ 2022/04/1_contained.exs
@@ -0,0 +1,13 @@
+#!/usr/bin/env elixir
+
+line_regex = ~r/^(?<a_from>\d+)-(?<a_to>\d+),(?<b_from>\d+)-(?<b_to>\d+)/
+
+IO.stream()
+|> Stream.map(&Regex.named_captures(line_regex, &1))
+|> Stream.filter(fn %{"a_from" => a_from, "a_to" => a_to, "b_from" => b_from, "b_to" => b_to} ->
+  a = MapSet.new(String.to_integer(a_from)..String.to_integer(a_to))
+  b = MapSet.new(String.to_integer(b_from)..String.to_integer(b_to))
+  MapSet.subset?(a, b) or MapSet.subset?(b, a)
+end)
+|> Enum.count()
+|> IO.puts()
